a3e1f88650d30132bd37d9f03d2ad449b0df276b
enigmaz <enigmatracker@gmail.com>
2021-08-23 16:02:16 +0800

    add element plus

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloElement msg="Hello Vue 3.0 + Element Plus + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import HelloElement from './components/HelloElement.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloElement
   }
 })
 </script>
